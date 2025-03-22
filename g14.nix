d2463f555b0b4a8d8bdf44f9faa276b496fb42d2
chuu-p <chuu801@pm.me>
2025-03-22 16:55:52 +0100

    16 current  2025-03-22 16:55:49  24.11.715880.b75693fb46bf  6.6.83                          *

--- g14.nix
+++ g14.nix
@@ -1,49 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
-}: let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in {
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
+}: {
+  imports = [];
 
-  # home-manager.users.my_username = {
-  /*
-  The home.stateVersion option does not have a default and must be set
-  */
-  # home.stateVersion = "18.09";
-  /*
-  Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
-  */
-  # };
-
-  # boot.kernelParams = [ "nomodeset" ];
-
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -79,19 +50,15 @@ in {
     };
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "de";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "de";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -99,57 +66,44 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.chuu = {
     isNormalUser = true;
     description = "chuu";
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
-      #  thunderbird
     ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     git
     alejandra
-    #  wget
     keepassxc
     chromium
     vscodium
     neofetch
-    pkgs.home-manager
+    mpv
   ];
 
-  # programs.git = {
-  #   enable = true;
-  #   userName  = "chuu-p";
-  #   userEmail = "chuu801@pm.me";
-  #aliases = {
-  #  c = "commit";
-  #  co = "checkout";
-  #  s = "status";
-  #};
-  # };
+  programs.git = {
+    enable = true;
+    # extraConfig = {
+    #   alias = {
+    #     st = "status";
+    #     co = "checkout";
+    #     br = "branch";
+    #     ci = "commit";
+    #     lol = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+    #     last = "log -1 HEAD --stat";
+    #   };
+    # };
+  };
 
   programs.bash.shellAliases = {
     l = "ls -alh";
@@ -159,16 +113,6 @@ in {
     g = "git";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
@@ -177,17 +121,5 @@ in {
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
