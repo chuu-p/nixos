35826f6c8b1072dccf91e7d971ddfcb081d36e04
chuu-p <chuu801@pm.me>
2025-03-28 10:14:05 +0100

    79 current  2025-03-28 10:13:59  25.05.20250322.1e5b653     6.12.19                          *

--- flake.nix
+++ flake.nix
@@ -3,17 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    home-manager,
   }: {
     nixosConfigurations.g14 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         ./hyper/flake.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.users.chuu = {
+            imports = [
+              ./home.nix # We'll create this file later
+            ];
+          };
+        }
       ];
     };
   };
