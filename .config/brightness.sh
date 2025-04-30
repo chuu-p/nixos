23a95113a3c898882c2e258a2eb1e50d667d83eb
chuu-p <chuu801@pm.me>
2025-04-30 09:28:19 +0200

    Add logging to brightness.sh :sparkles:

--- .config/brightness.sh
+++ .config/brightness.sh
@@ -5,12 +5,15 @@
 if [ "$1" = period-changed ]; then
 	case $3 in
 		night)
+		  echo "$(date +%T): redshift set night" >> /var/log/redshift.log
       brightnessctl s 0%
 			;;
 		transition)
+		  echo "$(date +%T): redshift set transition" >> /var/log/redshift.log
       brightnessctl s 50%
 			;;
 		daytime)
+		  echo "$(date +%T): redshift set daytime" >> /var/log/redshift.log
       brightnessctl s 100%
 			;;
 	esac
