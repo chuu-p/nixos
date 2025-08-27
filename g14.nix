a4f79ed642afa08599a5c61a7f4244079d82278f
chuu-p <chuu801@pm.me>
2025-08-27 16:17:29 +0200

    292 current  2025-08-27 16:17:25  25.05.20250322.1e5b653  6.12.19                          *

--- g14.nix
+++ g14.nix
@@ -34,15 +34,27 @@
     SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", MODE="0666"
   '';
 
-  services.acpid = {
+  services.xserver.xautolock = {
     enable = true;
-    handlers = {
-      button-prog1 = {
-        event = "button/prog1 PROG1";
-        action = "/home/chuu/git/nixos/PATH/c-debug-notification";
-      };
-    };
+    locker = "${pkgs.i3lock}/bin/i3lock -i /home/chuu/git/nixos/wallpapers/cirno_nix.png";
+    # Wait 10 minutes before locking
+    time = 10;
+    # This command prevents locking when a media player is active
+    # It looks for players using a tool called playerctl
+    # detectPlayers = "playerctl -a status 2>&1 | grep -q 'Playing'";
+    # This option forces unlocking if the player stops or the screen is already unlocked
+    # lockOnPlayerStop = true;
   };
+
+  # services.acpid = {
+  #   enable = true;
+  #   handlers = {
+  #     button-prog1 = {
+  #       event = "button/prog1 PROG1";
+  #       action = "/home/chuu/git/nixos/PATH/c-debug-notification";
+  #     };
+  #   };
+  # };
 
   musnix.enable = true;
 
