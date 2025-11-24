25d5ee63d13f3f2ade3bc65b538b599de1722a4a
chuu-p <chuu801@pm.me>
2025-11-24 14:47:46 +0100

    ✨ (flake.nix): add shell aliases for commonly used commands to home-manager configuration

--- flake.nix
+++ flake.nix
@@ -41,6 +41,20 @@
         {
           home-manager.backupFileExtension = "hm-backup";
           home-manager.users.chuu = {
+            home = {
+              shellAliases = {
+                l = "ls -alh";
+                ll = "ls -l";
+                ls = "ls --color=tty";
+                kubectl = "sudo k3s kubectl";
+                sudo = "sudo ";
+                prettier = "npx prettier --write";
+                g = "git";
+                cg = "cargo";
+                j = "just";
+                zj = "zellij";
+              };
+            };
             imports = [
               ./home.nix
             ];
