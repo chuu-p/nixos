af991358a85ff39cc5f2d414e52f6de2aeaa1151
chuu-p <chuu801@pm.me>
2026-02-07 21:13:52 +0100

    backup: 2026-02-07 21:13:52

--- homes/chuu/home.nix
+++ homes/chuu/home.nix
@@ -138,6 +138,9 @@ in {
         fish_default_key_bindings -M insert
         fish_vi_key_bindings --no-erase
       end
+      function fish_user_key_bindings
+        bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+      end
       set -g fish_key_bindings fish_hybrid_key_bindings
       fish_user_key_bindings
     '';
