9f11f664f05fcc89fe8d012511fce91bb5ffd21b
chuu-p <chuu801@pm.me>
2026-03-10 10:24:06 +0100

    ♻️ (config): remove unused sway/i3status scripts and config, add i3status config for sway

--- homes/_shared/sway/i3status_1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# shell script to prepend i3status with more stuff
-
-i3status -c ~/git/nixos/homes/_shared/sway/i3status.conf | while IFS= read -r line
-do
-        bt_output=$(~/git/nixos/homes/_shared/sway/bt-battery-dbus.sh DB:0E:33:43:5D:BD)
-        echo "${bt_output} | ${line}"
-done
