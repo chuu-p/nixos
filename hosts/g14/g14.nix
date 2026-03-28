9d5ce30b090dfabcb152c192cff092cb9b55b676
chuu-p <chuu801@pm.me>
2026-03-28 19:22:47 +0100

    🔧 (g14.nix): configure Nix garbage collection and store optimization

--- hosts/g14/g14.nix
+++ hosts/g14/g14.nix
@@ -88,6 +88,19 @@
     ];
     distributedBuilds = true;
   };
+
+  # collect garbage automatically, every week
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+
+  # deduplicate store files
+  nix.settings.auto-optimise-store = true;
+
+  # keep store blobs for old generations up to 30 days
+  nix.gc.options = "--delete-older-than 30d";
+
+  # only keep the last five generations (otherwise boot partition can fill up too much)
+  boot.loader.systemd-boot.configurationLimit = 5;
   documentation.man.generateCaches = false;
 
   # This is needed for Slippi to run.
